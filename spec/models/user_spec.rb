112559f22a077ad5122d188acfd0ed1d4003c8ac
Ranhiru Cooray <ranhiru@gmail.com>
2015-03-21 17:12:05 +0800

    WIP - Moved validations in to separate groups

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 describe User do
-  it { should validate_presence_of(:username) }
-  it { should validate_presence_of(:email_address) }
-  it { should validate_presence_of(:profile_pic_url) }
+  describe 'presence validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email_address) }
+    it { should validate_presence_of(:profile_pic_url) }
+  end
 
-  it { should have_many(:compliments) }
-  it { should have_many(:labels) }
-  it { should have_many(:comments) }
-  it { should have_many(:likes) }
+  describe 'have_many validations' do
+    it { should have_many(:compliments) }
+    it { should have_many(:labels) }
+    it { should have_many(:comments) }
+    it { should have_many(:likes) }
+  end
 end
