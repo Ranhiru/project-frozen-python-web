856e847b2606ba41e68dbb5178074aa0b4cd9133
Ranhiru Cooray <ranhiru@gmail.com>
2015-03-21 17:02:48 +0800

    WIP - Added validations for the has_many associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,9 @@ describe User do
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:email_address) }
   it { should validate_presence_of(:profile_pic_url) }
+
+  it { should have_many(:compliments) }
+  it { should have_many(:labels) }
+  it { should have_many(:comments) }
+  it { should have_many(:likes) }
 end
