f33f6c375736f3c120490d91fee7ec7c46ed1481
Ranhiru Cooray <ranhiru@gmail.com>
2015-03-21 16:06:01 +0800

    WIP - Added model associations

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,3 +1,5 @@
 class Label < ActiveRecord::Base
   belongs_to :user
+  has_many :compliment_labels
+  has_many :compliments, through: :compliment_labels
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_many :compliments
   has_many :labels
   has_many :comments
+  has_many :likes
 end
