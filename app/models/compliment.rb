19567a80f284e09153ecd14d6abc58b36a3ecaa9
Ranhiru Cooray <ranhiru@gmail.com>
2015-03-22 17:29:59 +0800

    WIP - Added validations and specs for compliment model

--- app/models/compliment.rb
+++ app/models/compliment.rb
@@ -4,4 +4,7 @@ class Compliment < ActiveRecord::Base
   has_many :likes
   has_many :compliment_labels
   has_many :labels, through: :compliment_labels
+
+  validates :compliment_text, :user, presence: true
+  validates_length_of :compliment_text, minimum: 3
 end
