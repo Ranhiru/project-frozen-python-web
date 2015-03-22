4db19275aebef13265d064a5c23db421eed77cf9
Ranhiru Cooray <ranhiru@gmail.com>
2015-03-22 19:28:45 +0800

    WIP - Added validations and specs for comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :compliment
   belongs_to :user
+
+  validates :comment_text, :user, :compliment, presence: true
+  validates_length_of :comment_text, minimum: 2
 end
