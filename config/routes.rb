2b9d5b08789c93f69b6d0f2b2cadf0168e8999be
Ranhiru Cooray <ranhiru@gmail.com>
2015-03-29 11:29:38 +0800

    WIP - Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get '/auth/:provider/callback', to: 'session#create'
+  get '/auth/:provider/callback', to: 'session#create', as: 'auth_callback'
+  get 'auth/failure', to: 'session#failure'
+  get 'signout', to: 'session#destroy', as: 'signout'
   get '/login', to: 'auth#login'
 end
