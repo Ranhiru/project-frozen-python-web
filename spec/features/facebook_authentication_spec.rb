04165030d6498d5936e736c8644554dd28a6abeb
Ranhiru Cooray <ranhiru@gmail.com>
2015-03-29 15:08:38 +0800

    WIP - Added feature spec for authentication

--- spec/features/facebook_authentication_spec.rb
+++ spec/features/facebook_authentication_spec.rb
@@ -1,16 +1,54 @@
 require "rails_helper"
 require_relative 'pages/home_page'
 
-describe "facebook authentication", :type => :feature do
+describe "authentication", :type => :feature do
 
-  let(:home_page) { HomePage.new }
+  before :all do
+    OmniAuth.config.test_mode = true
+  end
 
   before :each do
-    home_page.load
+    OmniAuth.config.mock_auth[:facebook] = nil
+    @home = HomePage.new
+    @home.load
+    expect(@home).to be_displayed
   end
 
   it 'should show a facebook login link' do
-    expect(home_page).to be_displayed
-    expect(home_page).to have_facebook_login_link
+    expect(@home).to have_facebook_login_link
+  end
+
+  describe 'facebook login link' do
+    let(:provider) { 'facebook' }
+    let(:uid) { '1234567890' }
+
+    before do
+      OmniAuth.config.mock_auth[:facebook] = auth_response
+      @home.facebook_login_link.click
+    end
+
+    describe 'when the user is already registered' do
+      let!(:user) { create(:user, :with_auth, id: uid) }
+      let!(:auth_response) { create(:auth_response, id: uid, auth_provider: 'facebook', username: user.username) }
+      let!(:no_of_users) { User.count }
+
+      it 'will redirect back to the user' do
+        expect(@home).to be_displayed
+      end
+
+      it 'will not show the facebook login link' do
+        expect(@home).to be_displayed
+        expect(@home).to_not have_facebook_login_link
+      end
+
+      it 'will show the users name' do
+        expect(@home).to have_welcome_message
+        expect(@home.welcome_message).to have_content user.username
+      end
+
+      it 'will not create a new user' do
+        expect(User.count).to eq no_of_users
+      end
+    end
   end
 end
