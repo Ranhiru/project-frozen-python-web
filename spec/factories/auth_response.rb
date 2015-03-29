faaeb8ab4fa7184f3cc953dac8cd116b0c1c48dc
Ranhiru Cooray <ranhiru@gmail.com>
2015-03-29 11:27:21 +0800

    WIP - Added new factory auth_response. Updated user factory

---
+++ spec/factories/auth_response.rb
@@ -0,0 +1,71 @@
+FactoryGirl.define do
+  # http://collectiveidea.com/blog/archives/2013/11/05/factory-girl-without-active-record/
+  factory :auth_response, class: OmniAuth::AuthHash do
+    skip_create
+
+    transient do
+      id { SecureRandom.random_number(1_000_000_000).to_s }
+      name { "#{first_name} #{last_name}" }
+      first_name "Joe"
+      last_name "Bloggs"
+      link { "http://www.facebook.com/#{username}" }
+      username "jbloggs"
+      location_id "123456789"
+      location_name "Palo Alto, California"
+      gender "male"
+      email "joe@bloggs.com"
+      timezone(-8)
+      locale "en_US"
+      verified true
+      updated_time { SecureRandom.random_number.seconds.ago }
+      token { SecureRandom.urlsafe_base64(100).delete("-_").first(100) }
+      expires_at { SecureRandom.random_number.seconds.from_now }
+      auth_provider { "facebook" }
+    end
+
+    provider { auth_provider }
+    uid { id }
+
+    info do
+      {
+        nickname: username,
+        email: email,
+        name: name,
+        first_name: first_name,
+        last_name: last_name,
+        image: "http://graph.facebook.com/#{id}/picture?type=square",
+        urls: { Facebook: link },
+        location: location_name,
+        verified: verified
+      }
+    end
+
+    credentials do
+      {
+        token: token,
+        expires_at: expires_at.to_i,
+        expires: true
+      }
+    end
+
+    extra do
+      {
+        raw_info: {
+          id: uid,
+          name: name,
+          first_name: first_name,
+          last_name: last_name,
+          link: link,
+          username: username,
+          location: { id: location_id, name: location_name },
+          gender: gender,
+          email: email,
+          timezone: timezone,
+          locale: locale,
+          verified: verified,
+          updated_time: updated_time.strftime("%FT%T%z")
+        }
+      }
+    end
+  end
+end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,17 @@ FactoryGirl.define do
     username { Faker::Internet.user_name }
     email_address { Faker::Internet.safe_email }
     profile_pic_url { Faker::Avatar.image(Faker::Internet.slug) }
+
+    transient do
+      id { SecureRandom.random_number(1_000_000_000).to_s }
+    end
   end
+
+  trait :with_auth do
+    uid { id }
+    provider = "facebook"
+    oauth_token { Faker::Number.number(10) }
+    oauth_expires_at { Faker::Date.between(10, 60) }
+  end
+
 end
