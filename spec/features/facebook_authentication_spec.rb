c5a34eadf70bc8eee16e260607c14ff43fb2cf9a
Ranhiru Cooray <ranhiru@gmail.com>
2015-04-03 10:10:34 +0800

    WIP - Added auth test for new users

--- spec/features/facebook_authentication_spec.rb
+++ spec/features/facebook_authentication_spec.rb
@@ -21,6 +21,7 @@ describe "authentication", :type => :feature do
   describe 'facebook login link' do
     let(:provider) { 'facebook' }
     let(:uid) { '1234567890' }
+    let(:no_of_users) { User.count }
 
     before do
       OmniAuth.config.mock_auth[:facebook] = auth_response
@@ -29,8 +30,7 @@ describe "authentication", :type => :feature do
 
     describe 'when the user is already registered' do
       let!(:user) { create(:user, :with_auth, id: uid) }
-      let!(:auth_response) { create(:auth_response, id: uid, auth_provider: 'facebook', username: user.username) }
-      let!(:no_of_users) { User.count }
+      let(:auth_response) { create(:auth_response, id: uid, auth_provider: 'facebook', username: user.username) }
 
       it 'will redirect back to the user' do
         expect(@home).to be_displayed
@@ -47,7 +47,30 @@ describe "authentication", :type => :feature do
       end
 
       it 'will not create a new user' do
-        expect(User.count).to eq no_of_users
+        expect(no_of_users).to eq 1
+      end
+    end
+
+    describe 'when the user is not registered before' do
+      let(:auth_response) { create(:auth_response, id: uid, auth_provider: 'facebook') }
+
+      it 'will redirect back to the user' do
+        expect(@home).to be_displayed
+      end
+
+      it 'will not show the facebook login link' do
+        expect(@home).to be_displayed
+        expect(@home).to_not have_facebook_login_link
+      end
+
+      it 'will show the users name' do
+        new_user = User.first
+        expect(@home).to have_welcome_message
+        expect(@home.welcome_message).to have_content new_user.username
+      end
+
+      it 'will create a new user' do
+        expect(no_of_users).to eq 1
       end
     end
   end
